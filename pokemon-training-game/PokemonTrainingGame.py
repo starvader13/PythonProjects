213f2f8a274647b0f6a11072b5ae2e10eba6c314
starvader13 <ashutoshgupta1311@gmail.com>
2023-11-12 12:05:58 +0530

    Create method for finding max and min power every time a new pokemon is added
    Signed-off-by: starvader13 <ashutoshgupta1311@gmail.com>

--- pokemon-training-game/PokemonTrainingGame.py
+++ pokemon-training-game/PokemonTrainingGame.py
@@ -1,8 +1,10 @@
 class PokemonTrainingGame:
     def __init__(self):
         self._powers = []
-        self._min_power = None
-        self._max_power = None
+        self._user_choice = None
+        self._min_power = 0
+        self._max_power = 0
+        self._itr = 0
 
     @property
     def powers(self):
@@ -13,6 +15,20 @@ class PokemonTrainingGame:
         if not isinstance(power, int):
             raise Exception("The entered power should only be an integer.")
         self.powers.append(power)
+
+    @property
+    def user_choice(self):
+        return self._user_choice
+
+    @user_choice.setter
+    def user_choice(self, user_choice):
+        if user_choice != "C" and user_choice != "E":
+            raise Exception("The user_choice can only be either 'C' or 'E'")
+
+        if user_choice == "C":
+            self._user_choice = True
+        else:
+            self._user_choice = False
 
     @property
     def min_power(self):
@@ -35,4 +51,22 @@ class PokemonTrainingGame:
             raise Exception("The Minimum power being assigned is not an integer.")
 
         self._max_power = max_power
+
+    @property
+    def itr(self):
+        return self._itr
+
+    @itr.setter
+    def itr(self, itr):
+        self._itr = itr
+
+    def calculateMinimumMaximumPower(self):        
+        if self.min_power==0 and self.max_power==0 and len(self.powers)==1:
+            self.min_power = self.powers[0]
+            self.max_power = self.powers[0]
+        else:
+            print(self.itr)
+            self.min_power= min(self.min_power, self.powers[self.itr])
+            self.max_power= max(self.max_power, self.powers[self.itr])
         
+        self.itr += 1
--- pokemon-training-game/script.py
+++ pokemon-training-game/script.py
@@ -1,8 +1,20 @@
+from PokemonTrainingGame import PokemonTrainingGame
+
 def welcomeMessage():
     print("Welcome to the Pokemon Training Game", end="\n\n")
     print("You will get two options as you move forward.")
-    print("1. Catch The Pokemon")
-    print("2. Exit The Program")
+    print("1. Catch The Pokemon [C]")
+    print("2. Exit The Program [E]")
 
 if __name__ == "__main__":
     welcomeMessage()
+
+    game = PokemonTrainingGame()
+
+    while True:
+        # print("Please enter your choice [C/E]")
+        # game.user_choice = str(input())
+
+        game.powers = int(input())
+        game.calculateMinimumMaximumPower()
+        print(game.min_power, game.max_power)
